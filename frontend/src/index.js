59edb8b0454a49094d46ea37d26829fdaa0aca8b
Eng-Mohamed-Adham <pythonh37@gmail.com>
2024-10-26 12:01:47 +0200

    second command

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
+
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
