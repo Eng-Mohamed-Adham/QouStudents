4f6dde6ee0a22d2d082fb53664fdcffa4af30a65
Eng-Mohamed-Adham <pythonh37@gmail.com>
2024-10-27 10:35:54 +0200

    b

--- backend/config/dbConn.js
+++ backend/config/dbConn.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 
 
+DATABASE_URL = 'mongodb+srv://pythonh37:root@cluster0.xow51.mongodb.net/Alkebeer?retryWrites=true&w=majority&authSource=admin'
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.DATABASE_URL)
+        await mongoose.connect(DATABASE_URL)
     }catch(err) {
         console.log(err)
     }
--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl:'https://alkbeer-awwy.onrender.com',
+    baseUrl:'https://alkbeer-awwy-pqeo.onrender.com/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
